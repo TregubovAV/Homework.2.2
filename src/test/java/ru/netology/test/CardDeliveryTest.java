97d7884df41c81222e43725afb81993f2a989bb1
TregubovAV <5595@rambler.ru>
2025-03-16 23:38:51 +0700

    Исправления по замечаниям

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -1,36 +1,27 @@
 package ru.netology.test;
 
 import java.time.Duration;
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
-
 import org.openqa.selenium.Keys;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class) // Управление порядком тестов
 public class CardDeliveryTest {
 
-    @BeforeAll
-    static void setUpAll() {
-        Configuration.headless = true;
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
     }
 
     @Test
-    @Order(1) // Запускаем этот тест первым
     void shouldSendFormWithValidData() {
-        open("http://localhost:9999");
-
         $("[data-test-id=city] input").setValue("Казань");
 
         String planningDate = LocalDate.now().plusDays(3)
                 .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
-        $("[data-test-id=date] input").click();
-        $("[data-test-id=date] input").sendKeys(Keys.END);
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(planningDate);
 
@@ -50,17 +41,27 @@ public class CardDeliveryTest {
     }
 
     @Test
-    @Order(2) // Запускаем этот тест вторым
     void shouldSendFormWithComplexElements() {
-        open("http://localhost:9999");
-
-        // Выбор города через выпадающий список
         $("[data-test-id=city] input").setValue("Ка");
         $$(".menu-item").findBy(text("Казань")).click();
 
-        // Выбор даты через календарь
+        // Выбор даты через календарь с проверкой месяца
         $("[data-test-id=date] input").click();
-        $$(".calendar__day").findBy(text(String.valueOf(LocalDate.now().plusDays(7).getDayOfMonth()))).click();
+        LocalDate plannedDate = LocalDate.now().plusDays(7);
+        String day = String.valueOf(plannedDate.getDayOfMonth());
+        String expectedMonth = plannedDate.format(DateTimeFormatter.ofPattern("MMMM"));
+
+        // Проверяем, нужно ли переключать месяц
+        String currentMonth = $(".calendar__name").text();
+        if (!currentMonth.contains(expectedMonth)) {
+            // Проверяем, можно ли кликнуть по стрелке
+            if ($(".calendar__arrow").exists() && !$(".calendar__arrow").getAttribute("data-disabled").equals("true")) {
+                $(".calendar__arrow").click();
+            }
+        }
+
+        // Кликаем по нужному дню
+        $$(".calendar__day").findBy(text(day)).click();
 
         $("[data-test-id=name] input").setValue("Иван Петров");
         $("[data-test-id=phone] input").setValue("+79991112233");
@@ -74,7 +75,7 @@ public class CardDeliveryTest {
 
         $("[data-test-id=notification] .notification__content")
                 .shouldBe(visible, Duration.ofSeconds(15))
-                .shouldHave(text("Встреча успешно забронирована на " + 
-                        LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))));
+                .shouldHave(text("Встреча успешно забронирована на " +
+                        plannedDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))));
     }
 }
